f67ebae437d5b3c4c46bcc8b19b3a0dc4db74574
max.chen <amingccc@gmail.com>
2018-07-22 11:33:37 +0800

    搭建项目结构，公共抽取，登陆页修改

---
+++ admin/login.php
@@ -0,0 +1,55 @@
+<?php 
+function login(){
+
+  //1.接收并校验
+  //2.持久化
+  //3.响应
+  if (empty($_POST['email'])) {
+    $GLOBALS['message'] = '请填写邮箱';
+    return;
+  }
+  if (empty($_POST['password'])) {
+    $GLOBALS['message'] = '请填写密码';
+    return;
+  }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  login();
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+  <meta charset="utf-8">
+  <title>Sign in &laquo; Admin</title>
+  <link rel="stylesheet" href="/static/assets/vendors/bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="/static/assets/vendors/animate/animate.css">
+  <link rel="stylesheet" href="/static/assets/css/admin.css">
+</head>
+<body>
+  <div class="login">
+    <form class="login-wrap<?php echo isset($message) ? ' shake animated' : '' ?>" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" autocomplete="off" novalidate>
+      <img class="avatar" src="/static/assets/img/default.png">
+      <?php if (isset($message)): ?>
+      <!-- 有错误信息时展示 -->
+      <div class="alert alert-danger">
+        <strong>错误！</strong> <?php echo $message; ?>
+      </div>
+      <?php endif ?>
+      <div class="form-group">
+        <label for="email" class="sr-only">邮箱</label>
+        <input id="email" name="email" type="email" class="form-control" placeholder="邮箱" autofocus>
+      </div>
+      <div class="form-group">
+        <label for="password" class="sr-only">密码</label>
+        <input id="password" name="password" type="password" class="form-control" placeholder="密码">
+      </div>
+      <button class="btn btn-primary btn-block">登 录</button>
+    </form>
+  </div>
+</body>
+</html>
