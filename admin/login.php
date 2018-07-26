f255718a966ced8156687fbaeecfdf94cde18204
max.chen <amingccc@gmail.com>
2018-07-26 21:05:48 +0800

    logout

--- admin/login.php
+++ admin/login.php
@@ -69,6 +69,10 @@ function login(){
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   login();
 }
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['action'])&& $_GET['action']==='logout') {
+
+  unset($_SESSION['current_login_user']);
+}
 
 ?>
 
